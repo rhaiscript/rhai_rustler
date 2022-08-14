a41927565ac15b3df8fec46c40c100f2f3fdc101
Fabrizio Sestito <fabrizio.sestito@suse.com>
2022-08-14 08:34:27 +0200

    Add error type, improve eval/2 spec

--- lib/evalex.ex
+++ lib/evalex.ex
@@ -6,6 +6,40 @@ defmodule EvalEx do
 
   @type evalex_any :: number() | boolean() | String.t() | nil | [evalex_any()]
 
+  @type evalex_error ::
+          :wrong_operator_amount
+          | :wrong_function_argument_amount
+          | :expected_string
+          | :expected_int
+          | :expected_float
+          | :expected_number
+          | :expected_number_or_string
+          | :expected_boolean
+          | :expected_tuple
+          | :exepcted_fixed_length_tuple
+          | :expected_empty
+          | :append_to_leaf_node
+          | :precedence_violation
+          | :variable_identifier_not_found
+          | :function_identefier_not_found
+          | :type_error
+          | :wrong_type_combination
+          | :unmatched_l_brace
+          | :unmatched_r_brace
+          | :missing_operator_outside_of_brace
+          | :unmatched_partial_token
+          | :addition_error
+          | :subtraction_error
+          | :negation_error
+          | :multiplication_error
+          | :division_error
+          | :modulation_error
+          | :invaild_regex
+          | :context_not_mutable
+          | :illegal_escape_sequence
+          | :custom_message
+          | :unknown
+
   @doc """
   Evaluates the given expression and returns the result.
 
@@ -32,6 +66,6 @@ defmodule EvalEx do
       "Variable identifier is not bound to anything by context: \"b\"."}}
   """
   @doc since: "0.1.0"
-  @spec eval(String.t(), map()) :: {:ok, evalex_any()} | {:error, {atom(), String.t()}}
+  @spec eval(String.t(), map()) :: {:ok, evalex_any()} | {:error, {evalex_error(), String.t()}}
   def eval(expression, %{} = context \\ %{}), do: EvalEx.Native.eval(expression, context)
 end
