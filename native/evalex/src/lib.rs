f9fb19ea51994cc6bb86fc3e93abbda274ec70c8
Fabrizio Sestito <fabrizio.sestito@suse.com>
2022-08-15 18:59:05 +0200

    Chore: rename variabile

--- native/evalex/src/lib.rs
+++ native/evalex/src/lib.rs
@@ -6,17 +6,17 @@ use rustler::{Env, MapIterator, Term};
 
 #[rustler::nif]
 fn eval<'a>(env: Env<'a>, string: &str, context: Term<'a>) -> Result<Term<'a>, Term<'a>> {
-    let mut context_map = HashMapContext::new();
+    let mut hash_map_context = HashMapContext::new();
 
     MapIterator::new(context)
         .expect("Should be a map in the argument")
         .for_each(|(k, v)| {
             let key: String = k.decode().expect("Should be a string");
 
-            context_map.set_value(key, types::to_value(env, &v)).ok();
+            hash_map_context.set_value(key, types::to_value(env, &v)).ok();
         });
 
-    match eval_with_context_mut(string, &mut context_map) {
+    match eval_with_context_mut(string, &mut hash_map_context) {
         Ok(value) => Ok(types::from_value(env, &value)),
         Err(err) => Err(errors::to_error_tuple(env, err)),
     }
