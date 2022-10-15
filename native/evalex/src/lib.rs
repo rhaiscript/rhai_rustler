702c0c0d110ea9d414c809e89e816cb09d4ab9ee
Fabrizio Sestito <fabrizio.sestito@suse.com>
2022-10-15 15:07:50 +0200

    WIP

--- native/evalex/src/lib.rs
+++ native/evalex/src/lib.rs
@@ -6,6 +6,7 @@ use std::collections::HashMap;
 use evalexpr::{
     build_operator_tree, eval_with_context_mut, ContextWithMutableVariables, HashMapContext,
 };
+use rhai::{Dynamic, Engine, EvalAltResult, Scope};
 use rustler::{Env, ResourceArc, Term};
 
 struct PrecompiledExpression {
@@ -23,9 +24,80 @@ fn eval<'a>(
     expression: &str,
     context: HashMap<String, Term<'a>>,
 ) -> Result<Term<'a>, Term<'a>> {
+    // Create an 'Engine'
+    let engine = Engine::new();
+    let mut scope = Scope::new();
+
+    for (k, v) in &context {
+        scope.push_constant_dynamic(k, to_dynamic(env, v));
+    }
+
+    match engine.run_with_scope(&mut scope, expression) {
+        Ok(a) => dbg!(a),
+        Err(e) => {
+            dbg!(e);
+        }
+    };
+
     match eval_with_context_mut(expression, &mut build_hash_map_context(env, context)) {
         Ok(value) => Ok(types::from_value(env, &value)),
         Err(err) => Err(errors::to_error_tuple(env, err)),
+    }
+}
+
+fn to_dynamic<'a>(env: Env<'a>, term: &Term<'a>) -> Dynamic {
+    match Term::get_type(*term) {
+        rustler::TermType::Binary => term
+            .decode::<String>()
+            .map(Dynamic::from)
+            .expect("get_type() returned Binary but could not decode as string."),
+
+        rustler::TermType::Atom => term
+            .decode::<bool>()
+            .map(Dynamic::from)
+            .or_else(|_| {
+                if *term == rustler::types::atom::nil().to_term(env) {
+                    Ok(Dynamic::from(()))
+                } else {
+                    term.atom_to_string().map(Dynamic::from)
+                }
+            })
+            .expect("get_type() returned Atom but could not decode as string, boolean or empty."),
+        rustler::TermType::EmptyList => Dynamic::from(Vec::<Dynamic>::new()),
+        rustler::TermType::Exception => Dynamic::from(()),
+        rustler::TermType::Fun => Dynamic::from(()),
+        rustler::TermType::List => {
+            let items: Vec<Dynamic> = term
+                .decode::<Vec<Term>>()
+                .expect("get_type() returned List but could not decode as list.")
+                .iter()
+                .map(|item| to_dynamic(env, item))
+                .collect();
+
+            Dynamic::from(items)
+        }
+        rustler::TermType::Map => {
+            let mut object_map = rhai::Map::new();
+
+            for (k, v) in term
+                .decode::<HashMap<String, Term>>()
+                .expect("get_type() returned Number but could not decod Hashmap.")
+            {
+                object_map.insert(k.into(), to_dynamic(env, &v));
+            }
+            Dynamic::from(object_map)
+        }
+        rustler::TermType::Number => term
+            .decode::<i64>()
+            .map(Dynamic::from)
+            .or_else(|_| term.decode::<f64>().map(Dynamic::from))
+            .expect("get_type() returned Number but could not decode as integer or float."),
+
+        rustler::TermType::Pid => Dynamic::from(()),
+        rustler::TermType::Port => Dynamic::from(()),
+        rustler::TermType::Ref => Dynamic::from(()),
+        rustler::TermType::Tuple => todo!("Tuple"),
+        rustler::TermType::Unknown => Dynamic::from(()),
     }
 }
 
