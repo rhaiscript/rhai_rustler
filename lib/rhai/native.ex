d85d523b8440faf4a2e5dfd3dffc6b74b6afe6f5
Fabrizio Sestito <fabrizio.sestito@suse.com>
2023-01-16 17:29:36 +0100

    Use config to set force build (#22)

--- lib/rhai/native.ex
+++ lib/rhai/native.ex
@@ -19,19 +19,13 @@ defmodule Rhai.Native do
     otp_app: :rhai_rustler,
     crate: "rhai_rustler",
     base_url: "https://github.com/fabriziosestito/rhai_rustler/releases/download/v#{version}",
-    force_build: System.get_env("RHAI_RUSTLER_FORCE_BUILD") in ["1", "true"],
+    force_build:
+      Application.compile_env(:rustler_precompiled, [:force_build, :rhai_rustler], false) ||
+        System.get_env("RHAI_RUSTLER_FORCE_BUILD") in ["1", "true"],
     version: version,
     targets: targets
 
   def eval(_, _) do
-    :erlang.nif_error(:nif_not_loaded)
-  end
-
-  def eval_precompiled_expression(_, _) do
-    :erlang.nif_error(:nif_not_loaded)
-  end
-
-  def precompile_expression(_) do
     :erlang.nif_error(:nif_not_loaded)
   end
 end
