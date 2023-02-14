d4a46b4c50b6c70b9bd7dd4425105ff699949ffa
Alessio Biancalana <dottorblaster@gmail.com>
2023-02-14 16:29:54 +0100

    Wrap engine functions

--- native/rhai_rustler/src/engine.rs
+++ native/rhai_rustler/src/engine.rs
@@ -58,6 +58,104 @@ fn engine_compile(
 }
 
 #[rustler::nif]
+fn engine_set_allow_anonymous_fn(resource: ResourceArc<EngineResource>, enable: bool) {
+    let mut engine = resource.engine.try_lock().unwrap();
+
+    engine.set_allow_anonymous_fn(enable);
+}
+
+#[rustler::nif]
+fn engine_allow_anonymous_fn(resource: ResourceArc<EngineResource>) -> bool {
+    let engine = resource.engine.try_lock().unwrap();
+
+    engine.allow_anonymous_fn()
+}
+
+#[rustler::nif]
+fn engine_set_allow_if_expression(resource: ResourceArc<EngineResource>, enable: bool) {
+    let mut engine = resource.engine.try_lock().unwrap();
+
+    engine.set_allow_if_expression(enable);
+}
+
+#[rustler::nif]
+fn engine_allow_if_expression(resource: ResourceArc<EngineResource>) -> bool {
+    let engine = resource.engine.try_lock().unwrap();
+
+    engine.allow_if_expression()
+}
+
+#[rustler::nif]
+fn engine_set_allow_loop_expressions(resource: ResourceArc<EngineResource>, enable: bool) {
+    let mut engine = resource.engine.try_lock().unwrap();
+
+    engine.set_allow_loop_expressions(enable);
+}
+
+#[rustler::nif]
+fn engine_allow_loop_expressions(resource: ResourceArc<EngineResource>) -> bool {
+    let engine = resource.engine.try_lock().unwrap();
+
+    engine.allow_loop_expressions()
+}
+
+#[rustler::nif]
+fn engine_set_allow_looping(resource: ResourceArc<EngineResource>, enable: bool) {
+    let mut engine = resource.engine.try_lock().unwrap();
+
+    engine.set_allow_looping(enable);
+}
+
+#[rustler::nif]
+fn engine_allow_looping(resource: ResourceArc<EngineResource>) -> bool {
+    let engine = resource.engine.try_lock().unwrap();
+
+    engine.allow_looping()
+}
+
+#[rustler::nif]
+fn engine_set_allow_shadowing(resource: ResourceArc<EngineResource>, enable: bool) {
+    let mut engine = resource.engine.try_lock().unwrap();
+
+    engine.set_allow_shadowing(enable);
+}
+
+#[rustler::nif]
+fn engine_allow_shadowing(resource: ResourceArc<EngineResource>) -> bool {
+    let engine = resource.engine.try_lock().unwrap();
+
+    engine.allow_shadowing()
+}
+
+#[rustler::nif]
+fn engine_set_allow_statement_expression(resource: ResourceArc<EngineResource>, enable: bool) {
+    let mut engine = resource.engine.try_lock().unwrap();
+
+    engine.set_allow_statement_expression(enable);
+}
+
+#[rustler::nif]
+fn engine_allow_statement_expression(resource: ResourceArc<EngineResource>) -> bool {
+    let engine = resource.engine.try_lock().unwrap();
+
+    engine.allow_statement_expression()
+}
+
+#[rustler::nif]
+fn engine_set_allow_switch_expression(resource: ResourceArc<EngineResource>, enable: bool) {
+    let mut engine = resource.engine.try_lock().unwrap();
+
+    engine.set_allow_switch_expression(enable);
+}
+
+#[rustler::nif]
+fn engine_allow_switch_expression(resource: ResourceArc<EngineResource>) -> bool {
+    let engine = resource.engine.try_lock().unwrap();
+
+    engine.allow_switch_expression()
+}
+
+#[rustler::nif]
 fn engine_set_fail_on_invalid_map_property(resource: ResourceArc<EngineResource>, enable: bool) {
     let mut engine = resource.engine.try_lock().unwrap();
 
@@ -69,4 +167,141 @@ fn engine_fail_on_invalid_map_property(resource: ResourceArc<EngineResource>) ->
     let engine = resource.engine.try_lock().unwrap();
 
     engine.fail_on_invalid_map_property()
+}
+
+#[rustler::nif]
+fn engine_set_fast_operators(resource: ResourceArc<EngineResource>, enable: bool) {
+    let mut engine = resource.engine.try_lock().unwrap();
+
+    engine.set_fast_operators(enable);
+}
+
+#[rustler::nif]
+fn engine_fast_operators(resource: ResourceArc<EngineResource>) -> bool {
+    let engine = resource.engine.try_lock().unwrap();
+
+    engine.fast_operators()
+}
+
+#[rustler::nif]
+fn engine_set_max_array_size(resource: ResourceArc<EngineResource>, max_size: usize) {
+    let mut engine = resource.engine.try_lock().unwrap();
+
+    engine.set_max_array_size(max_size);
+}
+
+#[rustler::nif]
+fn engine_max_array_size(resource: ResourceArc<EngineResource>) -> usize {
+    let engine = resource.engine.try_lock().unwrap();
+
+    engine.max_array_size()
+}
+
+#[rustler::nif]
+fn engine_set_max_call_levels(resource: ResourceArc<EngineResource>, levels: usize) {
+    let mut engine = resource.engine.try_lock().unwrap();
+
+    engine.set_max_call_levels(levels);
+}
+
+#[rustler::nif]
+fn engine_max_call_levels(resource: ResourceArc<EngineResource>) -> usize {
+    let engine = resource.engine.try_lock().unwrap();
+
+    engine.max_call_levels()
+}
+
+#[rustler::nif]
+fn engine_set_max_expr_depths(
+    resource: ResourceArc<EngineResource>,
+    max_expr_depth: usize,
+    max_function_expr_depth: usize,
+) {
+    let mut engine = resource.engine.try_lock().unwrap();
+
+    engine.set_max_expr_depths(max_expr_depth, max_function_expr_depth);
+}
+
+#[rustler::nif]
+fn engine_max_expr_depth(resource: ResourceArc<EngineResource>) -> usize {
+    let engine = resource.engine.try_lock().unwrap();
+
+    engine.max_expr_depth()
+}
+
+#[rustler::nif]
+fn engine_max_function_expr_depth(resource: ResourceArc<EngineResource>) -> usize {
+    let engine = resource.engine.try_lock().unwrap();
+
+    engine.max_function_expr_depth()
+}
+
+#[rustler::nif]
+fn engine_set_max_map_size(resource: ResourceArc<EngineResource>, max_size: usize) {
+    let mut engine = resource.engine.try_lock().unwrap();
+
+    engine.set_max_map_size(max_size);
+}
+
+#[rustler::nif]
+fn engine_max_map_size(resource: ResourceArc<EngineResource>) -> usize {
+    let engine = resource.engine.try_lock().unwrap();
+
+    engine.max_map_size()
+}
+
+#[rustler::nif]
+fn engine_set_max_modules(resource: ResourceArc<EngineResource>, modules: usize) {
+    let mut engine = resource.engine.try_lock().unwrap();
+
+    engine.set_max_modules(modules);
+}
+
+#[rustler::nif]
+fn engine_max_modules(resource: ResourceArc<EngineResource>) -> usize {
+    let engine = resource.engine.try_lock().unwrap();
+
+    engine.max_modules()
+}
+
+#[rustler::nif]
+fn engine_set_max_operations(resource: ResourceArc<EngineResource>, operations: u64) {
+    let mut engine = resource.engine.try_lock().unwrap();
+
+    engine.set_max_operations(operations);
+}
+
+#[rustler::nif]
+fn engine_max_operations(resource: ResourceArc<EngineResource>) -> u64 {
+    let engine = resource.engine.try_lock().unwrap();
+
+    engine.max_operations()
+}
+
+#[rustler::nif]
+fn engine_set_max_string_size(resource: ResourceArc<EngineResource>, max_len: usize) {
+    let mut engine = resource.engine.try_lock().unwrap();
+
+    engine.set_max_string_size(max_len);
+}
+
+#[rustler::nif]
+fn engine_max_string_size(resource: ResourceArc<EngineResource>) -> usize {
+    let engine = resource.engine.try_lock().unwrap();
+
+    engine.max_string_size()
+}
+
+#[rustler::nif]
+fn engine_set_strict_variables(resource: ResourceArc<EngineResource>, enable: bool) {
+    let mut engine = resource.engine.try_lock().unwrap();
+
+    engine.set_strict_variables(enable);
+}
+
+#[rustler::nif]
+fn engine_strict_variables(resource: ResourceArc<EngineResource>) -> bool {
+    let engine = resource.engine.try_lock().unwrap();
+
+    engine.strict_variables()
 }
