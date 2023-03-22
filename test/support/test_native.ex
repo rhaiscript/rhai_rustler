fcb23c14be0e4f2cc591b95f22ae45ce1b71ed3e
Fabrizio Sestito <fabrizio.sestito@suse.com>
2023-03-22 14:00:57 +0100

    Add moduledoc

--- test/support/test_native.ex
+++ test/support/test_native.ex
@@ -1,4 +1,10 @@
 defmodule Rhai.TestDylibModule do
+  @moduledoc """
+  This module is used to test the integration with rhai-dylib.
+
+  Rustler is needed to trigger the compilation of the crate, and it does not export NIFs.
+  """
+
   use Rustler,
     otp_app: :rhai_rustler,
     crate: :test_dylib_module
