e875fe8ab13a9fab1a6fdeeda38c09d57bd47178
Fabrizio Sestito <fabrizio.sestito@suse.com>
2023-03-24 19:57:13 +0100

    macOS extension workaround

--- test/support/test_native.ex
+++ test/support/test_native.ex
@@ -8,4 +8,16 @@ defmodule Rhai.TestDylibModule do
   use Rustler,
     otp_app: :rhai_rustler,
     crate: :test_dylib_module
+
+  if :os.type() == {:unix, :darwin} do
+    @after_compile __MODULE__
+
+    # This is a workaround since Rustler creates a .so file on macOS,
+    # but the dylib module resolver expects a .dylib file.
+    def __after_compile__(_, _) do
+      path = File.cwd!() <> "/priv/native/libtest_dylib_module"
+
+      File.ln_s(path <> ".so", path <> ".dylib")
+    end
+  end
 end
