a6abfb224a94d89d419760f86306007fee6adf7e
Fabrizio Sestito <fabrizio.sestito@suse.com>
2023-03-26 20:39:42 +0200

    Bind AST source-related methods

--- lib/rhai/native.ex
+++ lib/rhai/native.ex
@@ -90,6 +90,11 @@ defmodule Rhai.Native do
   def scope_set_alias(_scope, _name, _alias), do: err()
   def scope_set_or_push(_scope, _name, _value), do: err()
   def scope_iter_collect(_scope), do: err()
+  # AST
+  def ast_empty(), do: err()
+  def ast_source(_ast), do: err()
+  def ast_set_source(_ast, _source), do: err()
+  def ast_clear_source(_ast), do: err()
 
   defp err, do: :erlang.nif_error(:nif_not_loaded)
 end
--- native/rhai_rustler/src/ast.rs
+++ native/rhai_rustler/src/ast.rs
@@ -1,7 +1,36 @@
-use std::sync::RwLock;
+use std::sync::Mutex;
 
 use rhai::AST;
+use rustler::ResourceArc;
 
 pub struct ASTResource {
-    pub ast: RwLock<AST>,
+    pub ast: Mutex<AST>,
+}
+
+#[rustler::nif]
+fn ast_empty() -> ResourceArc<ASTResource> {
+    ResourceArc::new(ASTResource {
+        ast: Mutex::new(AST::empty()),
+    })
+}
+
+#[rustler::nif]
+fn ast_set_source(resource: ResourceArc<ASTResource>, source: &str) {
+    let mut ast = resource.ast.try_lock().unwrap();
+
+    ast.set_source(source);
+}
+
+#[rustler::nif]
+fn ast_clear_source(resource: ResourceArc<ASTResource>) {
+    let mut ast = resource.ast.try_lock().unwrap();
+
+    ast.clear_source();
+}
+
+#[rustler::nif]
+fn ast_source(resource: ResourceArc<ASTResource>) -> Option<String> {
+    let ast = resource.ast.try_lock().unwrap();
+
+    ast.source().map(|s| s.to_string())
 }
