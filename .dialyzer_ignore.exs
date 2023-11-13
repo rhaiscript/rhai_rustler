62624b2c14659e2e08c66502aee1d0ff262a8d0d
Fabrizio Sestito <fabrizio.sestito@suse.com>
2023-11-13 11:26:26 +0100

    fix: dialyzer
    Signed-off-by: Fabrizio Sestito <fabrizio.sestito@suse.com>

--- .dialyzer_ignore.exs
+++ .dialyzer_ignore.exs
@@ -2,4 +2,5 @@
   # something is wrong with the RustlerPrecompiled use macro
   # this fixes it by ignoring the Rhai.Native module completely
   {"lib/rhai/native.ex"},
+  {"test/support/test_dylib_module.ex"}
 ]
